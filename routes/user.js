d23789e596bbe47dab7864adae7347932c527bdb
El chueco <2840493+AlejandroJMelo@users.noreply.github.com>
2020-08-19 17:36:43 -0300

    commit base

---
+++ routes/user.js
@@ -0,0 +1,134 @@
+const express = require('express');
+const User = require('../models/user');
+const app = express();
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const user = require('../models/user');
+
+
+
+
+app.get('/user', function(req, res) {
+
+    let from = req.query.from || 0;
+    from = Number(from);
+
+    let to = req.query.to || 10;
+    to = Number(to);
+    User.find({ status: true }, 'name email role status img google')
+        .skip(from)
+        .limit(to)
+        .exec((err, users) => {
+            if (err) {
+                return res.status(400, json({
+                    ok: false,
+                    err
+                }))
+            }
+
+
+            User.countDocuments({ status: true }, (err, count) => {
+
+                res.json({
+                    ok: true,
+                    count: count,
+                    users
+                })
+            })
+
+        })
+
+})
+app.post('/user', function(req, res) {
+
+    let body = req.body;
+
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        user: body.user,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+
+    });
+
+    user.save((err, userDB) => {
+        if (err) {
+            return res.status(400, json({
+                ok: false,
+                err
+            }))
+        }
+
+        res.json({
+            ok: true,
+            user: userDB
+        })
+
+        console.log(`User ${userDB.name} saved `);
+    })
+})
+app.put('/user/:id', function(req, res) {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['name', 'email', 'role', 'status', 'img']);
+
+    User.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, userDB) => {
+        if (err) {
+            return res.status(400, json({
+                ok: false,
+                err
+            }))
+        }
+        res.json({
+            ok: true,
+            user: userDB
+        })
+    })
+
+})
+app.delete('/user/:id', function(req, res) {
+
+    // res.json({
+    //     ok: true
+    //         // user: '';
+    // })
+
+    // let id = req.params.id;
+
+    // User.findByIdAndRemove(id, (err, userDeleted) => {
+
+    //     if (err) {
+    //         return res.status(400, json({
+    //             ok: false,
+    //             err
+    //         }))
+    //     }
+
+
+    //     res.json({
+    //         ok: true,
+    //         userDelete: userDeleted
+    //     })
+
+    // })
+
+
+    let id = req.params.id;
+    let changeStatus = { status: false };
+
+    User.findByIdAndUpdate(id, changeStatus, { new: true }, (err, userDB) => {
+        if (err) {
+            return res.status(400, json({
+                ok: false,
+                err
+            }))
+        }
+        res.json({
+            ok: true,
+            user: userDB
+        })
+    })
+
+})
+
+module.exports = app;
