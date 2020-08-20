3cbb042e4645fad7c7a339c375aad3f5804f07ea
El chueco <2840493+AlejandroJMelo@users.noreply.github.com>
2020-08-20 19:14:37 -0300

    port

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 
+const port = process.env.PORT;
+
 app.get('/', function(req, res) {
     res.send('Hello World')
 })
 
-app.listen(80)
+app.listen(port || 80, () => {
+    console.log('Server started');
+})
